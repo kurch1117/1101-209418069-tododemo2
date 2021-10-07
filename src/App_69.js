d894273060089f9fc77b6972763727388cbe72fc
kurch1117 <zaq77998@gmail.com>
2021-10-07 12:39:51 +0800

    first commit

---
+++ src/App_69.js
@@ -0,0 +1,15 @@
+import Todo_69 from './components/Todo_69';
+import './App.css';
+
+function App_69() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_69 text='aaa'/>
+      <Todo_69 text='bbb'/>
+      <Todo_69 text='ccc'/>
+    </div>
+  );
+}
+
+export default App_69;
